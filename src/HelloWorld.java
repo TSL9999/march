7961b61e0e04d9d1e29c082dba457204849330e5
lthittaa <lit792@gmail.com>
2026-03-28 23:03:04 +0800

    Colleague123456

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public void sayHello() {
-        System.out.println("Hello,Colleague");
+        System.out.println("Hello,Colleague123456");
     }
     public void sayHelloAgain() {
         System.out.println("Hello Again");
