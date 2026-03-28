317aa5e51c653ec6ad0bd8c46bd51b017f0004e0
lthittaa <lit792@gmail.com>
2026-03-28 22:55:30 +0800

    Hello123

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public void sayHello() {
-        System.out.println("HelloA");
+        System.out.println("Hello123");
     }
     public void sayHelloAgain() {
         System.out.println("Hello Again");
